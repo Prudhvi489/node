1ce0b3850960303106297f4bef9e95147ed091c2
Prudhvi sai <prudhvisai@krify.com>
2023-10-16 18:02:35 +0530

    cors handling

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app=express();
 const bodyparser= require('body-parser');
 const router = require('./routes')
 const mongoose = require('mongoose');
+const cors=  require('cors')
 app.use(bodyparser.urlencoded({extended:false}));
+// accepting cors
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+    ],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 // parse application/json
 app.use(express.json({ limit: "10mb" }));
 // Testing api woking or not
